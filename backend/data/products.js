a8cf5625187738d4d902ef7280531ab2b4389c00
DennisEduard01 <dennis.eduard01@yahoo.com>
2023-04-13 00:00:11 +0200

    Add backend folder & initialize npm

---
+++ backend/data/products.js
@@ -0,0 +1,69 @@
+const products = [
+  {
+    _id: 1,
+    name: 'Fresh Yellow Onions',
+    image: '/images/onions.jpg',
+    description:
+      'Onions Net 1kg. The spicy taste of onions is indispensable in many dishes. Because of their pungent taste, they can be used as a condiment or as a vegetable.',
+    category: 'Vegetables',
+    price: 1.39,
+    countInStock: 10,
+  },
+
+  {
+    _id: 2,
+    name: 'Baked Bread',
+    image: '/images/bread.jpg',
+    description:
+      'Ideal bread for preparing a toasted sandwich or thick fresh sandwich. It is a half loaf that has been sliced ​​and packaged. The City Bakery. Exclusively available at Flink, for you, for the city. With our sleeves rolled up, we tackle and bake. The best bread and the tastiest goodies. Too many to mention but endless to enjoy.',
+    category: 'Bakery',
+    price: 1.29,
+    countInStock: 20,
+  },
+
+  {
+    _id: 3,
+    name: "Lay's Chips",
+    image: '/images/chips.jpg',
+    description:
+      "The one and only Lay's crisps, fried crispy, with a little salt for taste.",
+    category: 'Snacks',
+    price: 1.99,
+    countInStock: 7,
+  },
+
+  {
+    _id: 4,
+    name: 'Green Cucumber',
+    image: '/images/cucumber.jpg',
+    description:
+      'Cucumber. Cucumber has a fresh, crunchy, neutral taste. Delicious with a salad or as a healthy snack.',
+    category: 'Vegetables',
+    price: 1.09,
+    countInStock: 15,
+  },
+
+  {
+    _id: 5,
+    name: 'Panini Sandwich',
+    image: '/images/panini.jpg',
+    description:
+      'This panini is topped with delicious spicy cajun chicken, corn and jalapeño. The cheddar cheese goes perfectly with the chicken. A panini that leaves you wanting more',
+    category: 'Bakery',
+    price: 2.99,
+    countInStock: 10,
+  },
+
+  {
+    _id: 6,
+    name: 'Vine Tomatoes',
+    image: '/images/tomatoes.jpg',
+    description:
+      'Vine tomatoes have a fresh, sweet, firm flavour. They are packed with flavor because they are only harvested when the entire truss is ripe. That is why this tomato is perfect for salads.',
+    category: 'Vegetables',
+    price: 2.99,
+    countInStock: 12,
+  },
+];
+
+export default products;
