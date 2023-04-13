90e40cbbbed097e7f4a5f93083451528f7a5019a
DennisEduard01 <dennis.eduard01@yahoo.com>
2023-04-13 20:30:57 +0200

    Create GET routes for /products and /product/:id

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,26 @@
 const express = require('express');
 
 const app = express();
+const products = require('./data/products');
+
+app.get('/', (req, res) => {
+  res.send('API is running');
+});
+
+// Get all products route
+app.get('/api/products', (req, res) => {
+  res.json(products);
+});
+
+// Get single product by id
+app.get('/api/products/:id', (req, res) => {
+  const { id } = req.params;
+
+  // Find object with the same 'id' key as the endpoint parameter
+  const product = products.find(prod => prod._id == id);
+
+  res.send(product);
+});
 
 app.listen(5000, () => {
   console.log('Server running on port 5000');
