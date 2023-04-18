0edd61755c33cb5262c13d03380ade2027ee87a3
DennisEduard01 <dennis.eduard01@yahoo.com>
2023-04-18 23:50:56 +0200

    Modify store.js to use the local storage of the browser as well as specify an initial state for the cart reducer. If the cart is empty, the initial state is an empty array, otherwise the initial state is whatever was added in the cart

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,12 +4,30 @@
 import { configureStore } from '@reduxjs/toolkit';
 import productsReducer from './slices/productSlice';
 import { productSlice } from './slices/productSlice';
+import cartSliceReducer from './slices/cartSlice';
+
+// Retrieve the cartItems array from the browser's localStorage API.
+// The localStorage.getItem method is used to retrieve the serialized version of the array.
+// The JSON.parse method is used to convert the JSON string back to an array.
+// If there are no cart items in localStorage, set cartItemsFromLocalStorage to an empty array.
+// This line of code is used in the initialization of the Redux store to check if there are any
+// Previously saved cart items and load them if they exist.
+const cartItemsFromLocalStorage = localStorage.getItem('cartItems')
+  ? JSON.parse(localStorage.getItem('cartItems'))
+  : [];
+
+// Specify what the initial state of a state should be
+const initialState = {
+  cart: { cartItems: cartItemsFromLocalStorage },
+};
 
 const store = configureStore({
   reducer: {
     productsList: productsReducer,
     product: productSlice.reducer,
+    cart: cartSliceReducer,
   },
+  preloadedState: initialState,
 });
 
 export default store;
