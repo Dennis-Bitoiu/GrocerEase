7bfd089c94b34f987a6b3d60682958873c266f5f
DennisEduard01 <dennis.eduard01@yahoo.com>
2023-04-19 23:40:38 +0200

    Add Cart.js component to the header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { LinkContainer } from 'react-router-bootstrap';
 import { Navbar, Nav, Container } from 'react-bootstrap';
+import Cart from './Cart';
 
 const Header = () => {
   return (
@@ -18,18 +19,21 @@ const Header = () => {
 
         <Navbar.Toggle aria-controls='basic-navbar-nav' />
         <Navbar.Collapse id='basic-navbar-nav'>
-          <Nav className='ms-auto'>
+          <Nav
+            className='ms-auto'
+            style={{ display: 'flex', alignItems: 'center' }}
+          >
             {/* Wrapping the Nav.Link component into a LinkContainer stops the page from refreshing when accessing the page */}
-            <LinkContainer to='/cart'>
-              <Nav.Link>
-                <i className='fas fa-shopping-cart me-2'></i>Cart
-              </Nav.Link>
-            </LinkContainer>
-
-            <LinkContainer to='/login'>
+            <LinkContainer to='/login' style={{ fontSize: '1.2rem' }}>
               <Nav.Link href='/login'>
                 {' '}
                 <i className='fas fa-user me-2'></i>Sign In
+              </Nav.Link>
+            </LinkContainer>
+
+            <LinkContainer to='/cart' className='ms-3'>
+              <Nav.Link>
+                <Cart />
               </Nav.Link>
             </LinkContainer>
           </Nav>
