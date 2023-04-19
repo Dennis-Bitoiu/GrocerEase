02dd1e4f37594baba204f662de4d3171c2c99479
DennisEduard01 <dennis.eduard01@yahoo.com>
2023-04-19 23:38:56 +0200

    Create cart component for the shopping cart inside the header

---
+++ frontend/src/components/Cart.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Button, Col, Row } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
+
+function Cart() {
+  const cart = useSelector(state => state.cart);
+
+  const { cartItems } = cart;
+
+  return (
+    <Button
+      type='button'
+      className='btn px-3'
+      size='lg'
+      style={{ backgroundColor: 'hsl(162, 63%, 41%)' }}
+    >
+      <Row className='header-cart'>
+        <Col md={5}>
+          <i class='fa-solid fa-basket-shopping fa-lg'></i>
+        </Col>
+        <Col md={7} style={{ fontSize: '1.1rem' }}>
+          <div>
+            €
+            {cartItems
+              .reduce((acc, item) => acc + item.qty * item.price, 0)
+              .toFixed(2)}
+          </div>
+        </Col>
+      </Row>
+    </Button>
+  );
+}
+
+export default Cart;
