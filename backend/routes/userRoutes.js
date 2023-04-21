b2cda76cdb01efc17ee09234a94ebac51b7fe39a
DennisEduard01 <dennis.eduard01@yahoo.com>
2023-04-21 19:32:29 +0200

    Create routes for users API

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,17 @@
 import express from 'express';
 const router = express.Router();
-import { authUser } from '../controllers/userController.js';
+import {
+  authUser,
+  getUserProfile,
+  registerUser,
+} from '../controllers/userController.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 router.route('/login', authUser).post(authUser);
+
+// Adding the 'protect' middleware requires a user to be authenticated in order to access a route
+router.route('/profile').get(protect, getUserProfile);
+
+router.route('/').post(registerUser);
 
 export default router;
