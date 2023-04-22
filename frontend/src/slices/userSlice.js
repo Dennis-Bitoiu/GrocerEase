59d8da3f636e5ead45b941144964fcf2d5347356
DennisEduard01 <dennis.eduard01@yahoo.com>
2023-04-22 19:51:53 +0200

    Add user slice

---
+++ frontend/src/slices/userSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState: { userInfo: {}, loading: false, error: null },
+  reducers: {
+    userLoginRequest: state => {
+      state.loading = true;
+    },
+    userLoginSucces: (state, action) => {
+      state.userInfo = action.payload;
+      state.loading = false;
+    },
+    userLoginFail: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    userLogout: state => {
+      state.userInfo = {};
+    },
+  },
+});
+
+export const { userLoginRequest, userLoginSucces, userLoginFail, userLogout } =
+  userSlice.actions;
+
+export default userSlice.reducer;
