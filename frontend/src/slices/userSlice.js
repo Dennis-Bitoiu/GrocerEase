6ea7feb63068cd0fd94f377bdfd74d9d34b465ff
DennisEduard01 <dennis.eduard01@yahoo.com>
2023-04-23 19:01:40 +0200

    Add slice for user registration

--- frontend/src/slices/userSlice.js
+++ frontend/src/slices/userSlice.js
@@ -16,11 +16,32 @@ const userSlice = createSlice({
       state.error = action.payload;
     },
     userLogout: state => {
-      state.userInfo = {};
+      state.userInfo = null;
+    },
+  },
+});
+
+const userRegisterReducer = createSlice({
+  name: 'userRegister',
+  initialState: { userInfo: null, loading: false, error: null },
+  reducers: {
+    userRegisterRequest: state => {
+      state.loading = true;
+    },
+    userRegisterSucces: (state, action) => {
+      state.userInfo = action.payload;
+      state.loading = false;
+    },
+    userRegisterFail: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
     },
   },
 });
 
 export const { userLoginRequest, userLoginSucces, userLoginFail, userLogout } = userSlice.actions;
+
+export const { userRegisterRequest, userRegisterSucces, userRegisterFail } = userRegisterReducer.actions;
+export { userRegisterReducer };
 
 export default userSlice.reducer;
