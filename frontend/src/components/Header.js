18cad6d07804301f69ad86291cc3ed5be8f0f008
DennisEduard01 <dennis.eduard01@yahoo.com>
2023-04-23 19:00:17 +0200

    Add conditional rendering based on the login status of the user

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,17 +1,23 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { LinkContainer } from 'react-router-bootstrap';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
 import Cart from './Cart';
+import { logout } from '../actions/userActions';
 
 const Header = () => {
+  const dispatch = useDispatch();
+
+  const userLogin = useSelector(state => state.userLogin);
+
+  const { userInfo } = userLogin;
+  console.log(userInfo);
+  function logoutHandler() {
+    dispatch(logout());
+  }
+
   return (
-    <Navbar
-      className='py-2'
-      bg='primary'
-      variant='primary'
-      expand='lg'
-      collapseOnSelect
-    >
+    <Navbar className='py-2' bg='primary' variant='primary' expand='lg' collapseOnSelect>
       <Container>
         <LinkContainer to='/'>
           <Navbar.Brand className='brand'>GrocerEase</Navbar.Brand>
@@ -19,23 +25,30 @@ const Header = () => {
 
         <Navbar.Toggle aria-controls='basic-navbar-nav' />
         <Navbar.Collapse id='basic-navbar-nav'>
-          <Nav
-            className='ms-auto'
-            style={{ display: 'flex', alignItems: 'center' }}
-          >
+          <Nav className='ms-auto' style={{ display: 'flex', alignItems: 'center' }}>
             {/* Wrapping the Nav.Link component into a LinkContainer stops the page from refreshing when accessing the page */}
-            <LinkContainer to='/login' style={{ fontSize: '1.2rem' }}>
-              <Nav.Link href='/login'>
-                {' '}
-                <i className='fas fa-user me-2'></i>Sign In
-              </Nav.Link>
-            </LinkContainer>
 
             <LinkContainer to='/cart' className='ms-3'>
               <Nav.Link>
                 <Cart />
               </Nav.Link>
             </LinkContainer>
+
+            {userInfo ? (
+              <NavDropdown title={userInfo.name} id='username'>
+                <LinkContainer to='/profile'>
+                  <NavDropdown.Item>Profile</NavDropdown.Item>
+                </LinkContainer>
+                <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+              </NavDropdown>
+            ) : (
+              <LinkContainer to='/login' style={{ fontSize: '1.2rem' }}>
+                <Nav.Link href='/login'>
+                  {' '}
+                  <i className='fas fa-user me-2'></i>Sign In
+                </Nav.Link>
+              </LinkContainer>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>
