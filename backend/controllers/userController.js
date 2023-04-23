aebe0ba41cc44af891da1da77e4e97c287f79547
DennisEduard01 <dennis.eduard01@yahoo.com>
2023-04-23 19:28:11 +0200

    Add controller for updating the profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -50,6 +50,35 @@ const getUserProfile = AsyncHandler(async (req, res) => {
   }
 });
 
+// @description: Update the user profile
+// @route: PUT /api/users/profile
+// @access: Private (only a logged in user can access it)
+const updateUserProfile = AsyncHandler(async (req, res) => {
+  // req.user property was set in the authMiddleware.js
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
 // @description: Register a new user
 // @route: POST /api/users
 // @access: Public (Anyone can access it)
@@ -88,4 +117,4 @@ const registerUser = AsyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile, registerUser };
+export { authUser, getUserProfile, updateUserProfile, registerUser };
