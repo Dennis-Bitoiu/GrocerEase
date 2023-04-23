fd484ed846620e126a6ef4ece6f880ce327e01e3
DennisEduard01 <dennis.eduard01@yahoo.com>
2023-04-23 19:28:32 +0200

    Add the controller for updating the profile to the user routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,16 +1,12 @@
 import express from 'express';
 const router = express.Router();
-import {
-  authUser,
-  getUserProfile,
-  registerUser,
-} from '../controllers/userController.js';
+import { authUser, getUserProfile, registerUser, updateUserProfile } from '../controllers/userController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 router.route('/login', authUser).post(authUser);
 
 // Adding the 'protect' middleware requires a user to be authenticated in order to access a route
-router.route('/profile').get(protect, getUserProfile);
+router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile);
 
 router.route('/').post(registerUser);
 
