b429950283b537912ae7a8492601f6b00406671f
DennisEduard01 <dennis.eduard01@yahoo.com>
2023-04-24 19:49:53 +0200

    Add the order routes to server.js

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
 
 dotenv.config();
 
@@ -27,6 +28,7 @@ app.get('/', (req, res) => {
 // Any incoming HTTP requests with the route prefix /api/products will be handled by the productRoutes middleware function
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
 
 // ERROR HANDLER MIDDLEWARES
 
