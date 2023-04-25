fd5ca1762f90d61ce6d20a73fab3f6c799dd6c28
DennisEduard01 <dennis.eduard01@yahoo.com>
2023-04-25 20:14:36 +0200

    Create controller to update the payment status of an order and to update the paymentResult that comes back from the PayPal API

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -54,4 +54,29 @@ const getOrderById = AsyncHandler(async (req, res) => {
   }
 });
 
-export { addOrderItems, getOrderById };
+// @description: Update order to 'paid'
+// @route: PUT /api/orders/:id
+// @access: Private (Only logged in users can access it)
+const updateOrderToPaid = AsyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email: req.body.payer.email_address,
+    };
+
+    const updatedOrder = await order.save();
+
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error('Order not found!');
+  }
+});
+
+export { addOrderItems, getOrderById, updateOrderToPaid };
