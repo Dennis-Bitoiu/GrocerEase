3c1fadc6bf43de2f1ce48d4a68c2d5b936b39690
DennisEduard01 <dennis.eduard01@yahoo.com>
2023-04-25 18:35:26 +0200

    Add Redux action to retrieve order details

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -3,6 +3,9 @@ import {
   orderCreateRequest,
   orderCreateSuccess,
   orderCreateFail,
+  orderDetailsRequest,
+  orderDetailsSuccess,
+  orderDetailsFail,
 } from '../slices/orderSlice';
 
 export const createOrder = order => async (dispatch, getState) => {
@@ -33,6 +36,41 @@ export const createOrder = order => async (dispatch, getState) => {
     const customMessage = error.response.data.message;
     dispatch(
       orderCreateFail(
+        error.response && customMessage ? customMessage : error.message
+      )
+    );
+  }
+};
+
+// Retrieve order details from the server for a given order ID.
+// Dispatch an action to show that the request is being made
+// Dispatch a success action with the fetched data or a fail action with the error message.
+export const getOrderDetails = id => async (dispatch, getState) => {
+  try {
+    dispatch(orderDetailsRequest());
+
+    // Retrieve the userInfo object from the 'userLogin' state
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        // Pass token through the authorization header
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    const { data } = await axios.get(
+      `http://localhost:5000/api/orders/${id}`,
+      config
+    );
+
+    dispatch(orderDetailsSuccess(data));
+  } catch (error) {
+    const customMessage = error.response.data.message;
+    dispatch(
+      orderDetailsFail(
         error.response && customMessage ? customMessage : error.message
       )
     );
