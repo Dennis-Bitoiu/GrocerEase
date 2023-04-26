71a087aef8c0194f2a7e81d671014e96ee28c44a
DennisEduard01 <dennis.eduard01@yahoo.com>
2023-04-26 18:53:19 +0200

    Dispatch action to retrieve all the orders of a used. Render a table with all the orders

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Form, Button, Row, Col } from 'react-bootstrap';
+import { Form, Button, Row, Col, Table } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
 import { getUserDetails, updateUserProfile } from '../actions/userActions';
+import { listMyOrders } from '../actions/orderActions';
 
 function ProfileScreen() {
   const [name, setName] = useState('');
@@ -27,6 +29,9 @@ function ProfileScreen() {
   const userUpdateProfile = useSelector(state => state.userUpdateProfile);
   const { success } = userUpdateProfile;
 
+  const myOrders = useSelector(state => state.myOrdersReducer);
+  const { orders, loading: loadingOrders, error: errorOrders } = myOrders;
+
   useEffect(() => {
     if (!userInfo) {
       navigate('/login');
@@ -37,9 +42,14 @@ function ProfileScreen() {
         // On the re-render, the setName and setEmail function will be called to set the email and the name values
         // To the ones corresponding the `user` object
         dispatch(getUserDetails('profile'));
+        dispatch(listMyOrders());
       } else {
         setName(user.name);
         setEmail(user.email);
+
+        // If the user is set, dispatch listMyOrders again in case a new order was placed
+        // dispatch(orderListMyReset());
+        dispatch(listMyOrders());
       }
     }
   }, [dispatch, navigate, userInfo, user]);
@@ -118,6 +128,57 @@ function ProfileScreen() {
       </Col>
       <Col md={9}>
         <h2>My Orders</h2>
+        {loadingOrders ? (
+          <Loader />
+        ) : errorOrders ? (
+          <Message variant='danger'>{errorOrders}</Message>
+        ) : (
+          <Table striped bordered hover responsive className='table-sm'>
+            <thead>
+              <tr>
+                <th> ID </th>
+                <th> Date </th>
+                <th> Total </th>
+                <th> Paid </th>
+                <th> Delivered </th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {orders.map(order => (
+                <tr key={order._id}>
+                  <td>{order._id}</td>
+                  <td>{order.createdAt.substring(0, 10)}</td>
+                  <td>€{order.totalPrice}</td>
+                  <td>
+                    {order.isPaid ? (
+                      order.paidAt.substring(0, 10)
+                    ) : (
+                      <i className='fas fa-times' style={{ color: 'red' }}></i>
+                    )}
+                  </td>
+                  <td>
+                    {order.isDelivered ? (
+                      <i class='fa-solid fa-check fa-lg'></i>
+                    ) : (
+                      <i
+                        className='fas fa-times fa-xl'
+                        style={{ color: 'red' }}
+                      ></i>
+                    )}
+                  </td>
+                  <td>
+                    <LinkContainer to={`/order/${order._id}`}>
+                      <Button variant='light' className='btn-sm'>
+                        Details
+                      </Button>
+                    </LinkContainer>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        )}
       </Col>
     </Row>
   );
