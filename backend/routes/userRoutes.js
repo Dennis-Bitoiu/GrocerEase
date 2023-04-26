d9c2b5034d50f3cc4b2453ec15031ed630fdc1e2
DennisEduard01 <dennis.eduard01@yahoo.com>
2023-04-26 19:21:11 +0200

    Create route to /api/users which requires an user to be logged in and an admin

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,23 @@
 import express from 'express';
 const router = express.Router();
-import { authUser, getUserProfile, registerUser, updateUserProfile } from '../controllers/userController.js';
-import { protect } from '../middleware/authMiddleware.js';
+import {
+  authUser,
+  getUserProfile,
+  getUsers,
+  registerUser,
+  updateUserProfile,
+} from '../controllers/userController.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
+
+router.route('/').get(protect, admin, getUsers);
 
 router.route('/login', authUser).post(authUser);
 
 // Adding the 'protect' middleware requires a user to be authenticated in order to access a route
-router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile);
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 
 router.route('/').post(registerUser);
 
