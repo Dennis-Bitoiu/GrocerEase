dedd0ef4564fc84b67c249848548e9fac253a05b
DennisEduard01 <dennis.eduard01@yahoo.com>
2023-04-27 14:39:31 +0200

    Dispatch action to delete user from the database

--- frontend/src/screens/UsersListScreen.js
+++ frontend/src/screens/UsersListScreen.js
@@ -5,7 +5,7 @@ import { LinkContainer } from 'react-router-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
-import { listUsers } from '../actions/userActions';
+import { listUsers, deleteUser } from '../actions/userActions';
 
 function UsersListScreen() {
   const dispatch = useDispatch();
@@ -17,16 +17,21 @@ function UsersListScreen() {
   const userLogin = useSelector(state => state.userLogin);
   const { userInfo } = userLogin;
 
+  const userDelete = useSelector(state => state.userDelete);
+  const { success: successDelete } = userDelete;
+
   useEffect(() => {
     if (userInfo && userInfo.isAdmin) {
       dispatch(listUsers());
     } else {
       navigate('/login');
     }
-  }, [dispatch, navigate]);
+  }, [dispatch, navigate, successDelete]);
 
   function deleteHandler(id) {
-    console.log(`Delete ${id}`);
+    if (window.confirm('Are you sure')) {
+      dispatch(deleteUser(id));
+    }
   }
 
   return (
