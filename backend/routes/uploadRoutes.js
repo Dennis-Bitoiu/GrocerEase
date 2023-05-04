e385f79b70342be38dfb232ba021e26b4dac62d5
DennisEduard01 <dennis.eduard01@yahoo.com>
2023-05-04 18:05:50 +0200

    Create config variables and route to handle POST request for uploading images

---
+++ backend/routes/uploadRoutes.js
@@ -0,0 +1,49 @@
+import express from 'express';
+import path from 'path';
+import multer from 'multer';
+
+const router = express.Router();
+
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename(req, file, cb) {
+    // Format the name of the file
+    // path.extname() extracts the extension of the original filename
+    cb(null, `${file.fieldname}-${Date.now}${path.extname(file.originalname)}`);
+  },
+});
+
+function checkFileType(file, cb) {
+  const filetypes = /jpg|jpeg|png/;
+
+  // Test the file extension on the filetypes expression
+  // Return true if the extension of the file is jpg, jpeg or png
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+
+  // Check the mime type of the file to make sure that the file submitted is an image
+  // And not something else
+  const mimetype = filetypes.test(file.mimetype);
+
+  if (extname && mimetype) {
+    return cb(null, true);
+  } else {
+    cb('Images only');
+  }
+}
+
+const upload = multer({
+  storage,
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  },
+});
+
+// Route handler for single file uploads using Multer middleware
+// Returns the file path as the response
+router.post('/', upload.single('image'), (req, res) => {
+  res.send(`/${req.file.path}`);
+});
+
+export default router;
