f916222e8a1f97a1d6aa86e8dac150dbc428bfe4
DennisEduard01 <dennis.eduard01@yahoo.com>
2023-05-07 18:45:13 +0200

    Modify HomeScreen to use the keyword parameter when dispatching the fetchProducts action

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,4 +1,6 @@
 import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
 import { useDispatch, useSelector } from 'react-redux';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
@@ -9,21 +11,26 @@ import Categories from '../components/Categories';
 const HomeScreen = () => {
   // Initializes the dispatch constant using the useDispatch hook from Redux.
   const dispatch = useDispatch();
+  const params = useParams();
+
+  // Destructure the keyword parameter
+  // Used when users search for a product
+  const { keyword } = params;
 
   // Dispatches the fetchProducts action when the component mounts.
   // The "dispatch" function is added as a dependency to the dependency array
   // To prevent the "useEffect" hook from causing an infinite loop.
   // Get products from the reducer and store them into the 'productsList' state
   useEffect(() => {
-    dispatch(fetchProducts());
-  }, [dispatch]);
+    dispatch(fetchProducts(keyword));
+  }, [dispatch, keyword]);
 
   const producstList = useSelector(state => state.productsList);
   const { products, loading, error } = producstList;
 
   return (
     <>
-    <Categories></Categories>
+      <Categories></Categories>
       <h1>Latest products</h1>
       {loading ? (
         <Loader />
