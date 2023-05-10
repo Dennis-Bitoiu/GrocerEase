7714f7cacb9c23235e2975f52447186094888889
DennisEduard01 <dennis.eduard01@yahoo.com>
2023-05-10 19:27:16 +0200

    Create a model for categories

---
+++ backend/models/categoriesModel.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+const categoriesSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+
+  //  Second argument, options of the schema
+  {
+    timestamps: true,
+  }
+);
+
+const Category = mongoose.model('Category', categoriesSchema);
+
+export default Category;
