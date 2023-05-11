8869537c093a43481ddf2d36425592ad03eafd59
DennisEduard01 <dennis.eduard01@yahoo.com>
2023-05-11 20:27:16 +0200

    Dispatch call to the API to retrieve all the categories and render an ItemMenu element for each category in the array that comes back from the API call

--- frontend/src/components/Categories.js
+++ frontend/src/components/Categories.js
@@ -1,43 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
-import ItemMenu from './ItemMenu';
 import Container from 'react-bootstrap/Container';
+import ItemMenu from './ItemMenu';
+import axios from 'axios';
+
+async function fetchCategories() {
+  try {
+    const { data } = await axios.get('http://localhost:5000/api/categories');
+    return data;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+}
 
 function Categories() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function callFetch() {
+      const res = await fetchCategories();
+      setData(res);
+    }
+
+    callFetch();
+  }, []);
+
   return (
     <Container fluid>
       <h1 className='mb-2'>Categories</h1>
       <Carousel variant='dark' indicators={false} interval={null}>
         <Carousel.Item>
-          <ItemMenu></ItemMenu>
-          <ItemMenu></ItemMenu>
-          <ItemMenu></ItemMenu>
-          <ItemMenu></ItemMenu>
-          <ItemMenu></ItemMenu>
-          <ItemMenu></ItemMenu>
-          <ItemMenu></ItemMenu>
-          <ItemMenu></ItemMenu>
-          <ItemMenu></ItemMenu>
-          <ItemMenu></ItemMenu>
-          <ItemMenu></ItemMenu>
-          <ItemMenu></ItemMenu>
-          <ItemMenu></ItemMenu>
+          {data.slice(0, data.length / 2).map(item => (
+            <ItemMenu name={item.name} image={item.image} />
+          ))}
         </Carousel.Item>
 
         <Carousel.Item>
-          <ItemMenu></ItemMenu>
-          <ItemMenu></ItemMenu>
-          <ItemMenu></ItemMenu>
-          <ItemMenu></ItemMenu>
-          <ItemMenu></ItemMenu>
-          <ItemMenu></ItemMenu>
-          <ItemMenu></ItemMenu>
-          <ItemMenu></ItemMenu>
-          <ItemMenu></ItemMenu>
-          <ItemMenu></ItemMenu>
-          <ItemMenu></ItemMenu>
-          <ItemMenu></ItemMenu>
-          <ItemMenu></ItemMenu>
+          {data.slice(data.length / 2).map(item => (
+            <ItemMenu name={item.name} image={item.image} />
+          ))}
         </Carousel.Item>
       </Carousel>
     </Container>
